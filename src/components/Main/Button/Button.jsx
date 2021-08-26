dd559843fce9b05c31775c9c37c013efc55707df
Vitaliy Danylchuk <vgdanulchyk@gmail.com>
2021-08-26 20:53:35 +0300

    aded Button styles

--- src/components/Main/Button/Button.jsx
+++ src/components/Main/Button/Button.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styles from './Button.module.scss';
 
 const Button = props => {
   return (
     <div className={styles.buttonBody}>
-      <div className={styles.btn}>
-        <span className={styles.noselect}>{props.button.name}</span>
-        <div className={styles.circle}></div>
-      </div>
+      <Link to="/excursions" className={styles.lincStyles}>
+        <div className={styles.btn}>
+          <span className={styles.noselect}>{props.button.name}</span>
+          <div className={styles.circle}></div>
+        </div>
+      </Link>
     </div>
   );
 };
