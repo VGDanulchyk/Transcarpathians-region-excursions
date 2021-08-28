7e14d076f82f3ce880c24209460bd847ceec9af6
Vitaliy Danylchuk <vgdanulchyk@gmail.com>
2021-08-28 23:09:16 +0300

    aded test Gallery photo

--- src/components/Main/PhotoGallerySection/PhotoGallerySection.jsx
+++ src/components/Main/PhotoGallerySection/PhotoGallerySection.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import PhotoGallery from './PhotoGallery/PhotoGallery';
+import styles from './PhotoGallerySection.module.scss';
 
 const PhotoGallerySection = () => {
   return (
-    <section>
+    <section className={styles.wrapper}>
       <PhotoGallery />
     </section>
   );
