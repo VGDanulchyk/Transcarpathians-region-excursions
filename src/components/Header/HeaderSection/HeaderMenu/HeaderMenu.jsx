65c020b452a243089b337b2e950482ee5bec21d2
Vitaliy Danylchuk <vgdanulchyk@gmail.com>
2021-09-06 17:57:59 +0300

    menu refactor

--- src/components/Header/HeaderSection/HeaderMenu/HeaderMenu.jsx
+++ src/components/Header/HeaderSection/HeaderMenu/HeaderMenu.jsx
@@ -1,51 +1,47 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import styles from './HeaderMenu.module.scss';
 import { NavLink } from 'react-router-dom';
 
-const HeaderMenu = () => {
-  const [active, setActive] = useState(false);
-
-  const onMenuHandler = () => {
-    setActive(prev => !prev);
-  };
+const HeaderMenu = ({ active, setActive }) => {
   return (
     <div className={styles.container}>
-      <div onClick={onMenuHandler} className={styles.menuIcon}>
+      <div
+        className={active ? 'styles.menuIcon styles.menuActive' : 'styles.menuIcon'}
+        onClick={() => setActive(!active)}
+      >
         <span></span>
       </div>
 
-      {active && (
-        <nav className={styles.menuBody}>
-          <ul className={styles.menuList}>
-            <li>
-              <NavLink exact to="/" className={styles.menuLink} activeClassName={styles.active}>
-                Головна
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/about" className={styles.menuLink} activeClassName={styles.active}>
-                Про Себе
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/excursions" className={styles.menuLink} activeClassName={styles.active}>
-                Екскурсії
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/gallery" className={styles.menuLink} activeClassName={styles.active}>
-                Галерея
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/contacts" className={styles.menuLink} activeClassName={styles.active}>
-                Контакти
-              </NavLink>
-            </li>
-          </ul>
-        </nav>
-      )}
+      <nav className={styles.menuBody}>
+        <ul className={styles.menuList}>
+          <li>
+            <NavLink exact to="/" className={styles.menuLink} activeClassName={styles.active}>
+              Головна
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/about" className={styles.menuLink} activeClassName={styles.active}>
+              Про Себе
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/excursions" className={styles.menuLink} activeClassName={styles.active}>
+              Екскурсії
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/gallery" className={styles.menuLink} activeClassName={styles.active}>
+              Галерея
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/contacts" className={styles.menuLink} activeClassName={styles.active}>
+              Контакти
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 };
--- src/components/Header/HeaderSection/HeaderSection.jsx
+++ src/components/Header/HeaderSection/HeaderSection.jsx
@@ -1,13 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './HeaderSection.module.scss';
 import Logo from './Logo/Logo';
 import HeaderMenu from './HeaderMenu/HeaderMenu';
 
 const HeaderSection = () => {
+  const [menuActive, setMenuActive] = useState(false);
   return (
     <section className={styles.container}>
       <Logo />
-      <HeaderMenu />
+      <HeaderMenu active={menuActive} setActive={setMenuActive} />
     </section>
   );
 };
